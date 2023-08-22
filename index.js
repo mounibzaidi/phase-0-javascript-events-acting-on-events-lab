b938c4b007767fd97cdf4f8a50bc2acc889208a4
zaidi mounib <mounibzaidi@gmail.com>
2023-08-22 15:59:44 +0100

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.bottom = "0px";
+dodger.style.left = "0px";
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+});
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    const right = left + 40;
+
+
+    if (right < 400) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
